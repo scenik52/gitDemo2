575f7641a4484c76a3e9bcecf13069e5bbc1499b
scenik52 <scenik44.ac@gmail.com>
2020-02-11 23:54:08 -0500

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void posJira() {
+		System.out.println("posJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+}
